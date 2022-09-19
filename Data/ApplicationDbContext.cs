1f2280bfd18bc76f064069e91f1825b1eb8e8c76
BobiOne <BobiOne@BOBIONE-PC>
2022-09-20 01:10:57 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetStore.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [StringLength(256)]
+        public string Name { get; set; }
+
+        [StringLength(256)]
+        public string Position { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
